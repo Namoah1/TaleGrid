5af6f0eadaa8639040005d2e7013c22adab638fe
Namoah1 <namoah@macalester.edu>
2021-07-14 22:14:05 -0500

    Initial Commit

---
+++ TaleGrid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaleGrid
+//
+//  Created by Nana Adwoa Odeibea Amoah on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
